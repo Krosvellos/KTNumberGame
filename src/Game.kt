9abb92cdcf258dd9c19662c68a976eb6a2ff15d4
Krosvellos <daniel.abraham@seznam.cz>
2024-03-26 23:59:25 +0100

    Added basic logic. Right now an issue with switching in Game class.

--- src/Game.kt
+++ src/Game.kt
@@ -1,2 +1,33 @@
+import kotlin.random.Random
 class Game {
+
+        fun play() {
+
+            val randNum = Random.nextInt(1, 10)
+            var guess = 0
+            var attempts = 3
+
+
+            println("A number is chosen between 1 and 10! Guess the number:")
+
+            do {
+                for (i in 0 until attempts) {
+                    guess = readLine()?.toIntOrNull() ?: continue
+
+                    when {
+
+                        guess > randNum -> println("The number is higher! Try again.")
+                        guess < randNum -> println("The number is lower! Try again.")
+                        else -> println("That is correct!")
+                    }
+                    if(i == attempts) {
+                        println("Game over!")
+                        break
+                    }
+                }
+                }while (guess != randNum)
+
+
+
+        }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,6 @@
 fun main() {
-    println("This is a new Number Guessing Game in written with Kotlin!")
+    println("This is a new Number Guessing Game written with Kotlin!")
+    println()
+    val game = Game()
+    game.play()
 }
