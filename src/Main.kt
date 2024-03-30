76b813148d45e0493a1ec13fa3511cf39c87a482
Krosvellos <daniel.abraham@seznam.cz>
2024-03-30 23:52:51 +0100

    The game now offers the player an option to play again. - Added a while loop with the ability to stop the game using a readLine.

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,16 @@
 fun main() {
     println("This is a new Number Guessing Game written with Kotlin!")
     println()
-    val game = Game()
-    game.play()
+
+    while(true) {
+        val game = Game()
+        game.play()
+
+        println("Do you want to play again? Write yes/no")
+        val playAgain = readLine()?.trim()?.toLowerCase()
+        if (playAgain != "yes") {
+            println("Thanks for playing!")
+            break
+        }
+    }
 }
